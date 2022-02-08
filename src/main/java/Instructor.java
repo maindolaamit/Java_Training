463b9a7bccaa650019322eb93c5fe1eaa4e4901a
maindolaamit <maindola.amit@gmail.com>
2022-02-08 12:54:00 +0530

    Instructor-Book - excersise

---
+++ src/main/java/Instructor.java
@@ -0,0 +1,70 @@
+public class Instructor {
+    public long id;
+    public String name;
+    public String title;
+    public String department;
+    public Book[] books;
+
+    public Instructor(long id, String name, String title, String department, Book[] books) {
+        // your code
+    }
+
+    public Instructor(long id, String name, String title, String department) {
+        // your code
+    }
+
+    public Instructor(long id, String name, String title) {
+        // your code
+    }
+
+    public Instructor(long id, String name) {
+        // your code
+    }
+
+    /**
+     * This method should return the book title of the last element in the Books array.
+     * Books array can have any number of elements
+     *
+     * @return Book title of the last book in array
+     */
+    public String getMostRecentBookTitle() {
+        // your code
+    }
+
+    /**
+     * This method will update the Book instance at the specified index in the Books array with the specified book
+     * title.
+     * The method should also return the Book object which will have the old book for which title was updated.
+     *
+     * @param index Index in array to update.
+     * @param title New title of the book to update.
+     * @return Returns the old book object
+     */
+    public Book updateBook(int index, String title) {
+        // your code
+    }
+
+    /**
+     * This method updates the Book instance at the specified index in the Books array with the given Book object.
+     *
+     * @param index Index in array to update.
+     * @param book  New book object
+     * @return Returns the old book object
+     */
+    public Book updateBook(int index, Book book) {
+        // your code
+    }
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Java for Beginners");
+        Book book2 = new Book("Scala for Beginners");
+        Book book3 = new Book("Effective Python");
+
+        Instructor instructor = new Instructor(101, "John", "Assistant Professor", "Computer Science", new Book[]{book1, book2, book3});
+        System.out.println(instructor.getMostRecentBookTitle());
+        System.out.println("old book title: " + instructor.updateBook(1, "Effective C#").getTitle());
+
+        Book book4 = new Book("Introduction to Data Mining");
+        System.out.println("old book title: " + instructor.updateBook(1, book4).getTitle());
+    }
+}
