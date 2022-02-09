b347d6bc718b1dad3917f6a632be6ed03325631f
maindolaamit <maindola.amit@gmail.com>
2022-02-09 16:05:28 +0530

    exception handling - bufferedinputstream

--- src/main/java/io/FileHandling.java
+++ src/main/java/io/FileHandling.java
@@ -8,7 +8,32 @@ public class FileHandling {
         long start = System.currentTimeMillis();
         fileStreamCopyImage();
         long end = System.currentTimeMillis();
-        System.out.println("time in ms = " + (end - start));
+        System.out.println("Input stream time in ms = " + (end - start));
+
+        start = System.currentTimeMillis();
+        bufferedFileStreamCopyImage();
+        end = System.currentTimeMillis();
+        System.out.println("Buffered Stream time in ms = " + (end - start));
+    }
+
+    /**
+     * Copy an image using BufferedInputStream
+     */
+    private static void bufferedFileStreamCopyImage() {
+        //        Stream --> InputStream --> FilterInputStream --> BufferedInputStream
+        try {
+            BufferedInputStream input = new BufferedInputStream(new FileInputStream("src/main/resources/sleeping.jpg"));
+            BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream("image.jpg"));
+            int read = input.read();
+            while (read != -1) {
+                output.write(read);
+                read = input.read();
+            }
+            input.close();
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
