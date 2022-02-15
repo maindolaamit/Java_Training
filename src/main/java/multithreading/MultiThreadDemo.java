ca562d6eb90faa5c0a1ef517a7883cf031c00d44
maindolaamit <maindola.amit@gmail.com>
2022-02-15 16:41:34 +0530

    multi-threading executor

--- src/main/java/multithreading/MultiThreadDemo.java
+++ src/main/java/multithreading/MultiThreadDemo.java
@@ -2,8 +2,7 @@ package multithreading;
 
 import java.sql.Time;
 import java.util.ConcurrentModificationException;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class MultiThreadDemo {
 
@@ -12,6 +11,80 @@ public class MultiThreadDemo {
     }
 
     public static void main(String[] args) throws InterruptedException {
+//        threadDemo();
+        executorDemo();
+    }
+
+    /**
+     * Problems with Thread - fine for small scale applications,
+     * - thread creation and tear down is an expensive operation
+     * - high resource consumption
+     * - that we have no limitation on the thread
+     * <p>
+     * Executor framework which utilizes the thread from ThreadPool we can re-use threads from thread pool.
+     * utilizes work-queue - tasks (task in turn uses Thread)
+     * ThreadPool can have only limited number of threads.
+     */
+    private static void executorDemo() {
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println("availableProcessors = " + availableProcessors);
+
+        // create a Thread pool of size 2
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            Thread t3 =
+                    new Thread(() -> { // Anonymous class of Runnable interface
+                        try {
+                            System.out.println(String.format("Thread %s - %s", Thread.currentThread().getName(),
+                                    Thread.currentThread().getState()));
+                            int sleepTime = ThreadLocalRandom.current().nextInt(1, 5);
+                            Thread.sleep(TimeUnit.SECONDS.toMillis(sleepTime));
+                            System.out.println(String.format("Thread %s - Completed",
+                                    Thread.currentThread().getName()));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    });
+            t3.setName("Thread " + finalI);
+            executor.submit(t3);
+        }
+        System.out.println("Shutting down the executor service");
+
+
+        // to receive the result from a Thread we can use Callable Interface
+        // unlike Runnable interface they can return and value
+        try {
+            Future<Integer> future = executor.submit(new Callable<Integer>() {
+                @Override
+                public Integer call() throws Exception {
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+                    return 5;
+                }
+            });
+//        while (!future.isDone()) {
+//                Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+//        }
+            Integer integer;
+            integer = future.get();
+//            integer = future.get(25, TimeUnit.SECONDS);
+            System.out.println("integer received from callable = " + integer);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // Shutdown the service
+        executor.shutdown(); // wait for the threads to complete
+        try { // did not shut down the executor even after timeout
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void threadDemo() throws InterruptedException {
         // Create a thread view its state
         Thread t1 = new MyThread();
         t1.setName("Thread 1");
